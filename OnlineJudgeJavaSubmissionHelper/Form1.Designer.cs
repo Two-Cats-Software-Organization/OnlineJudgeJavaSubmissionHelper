02cf7521eb35d1eea0c3ad161152ce6ab37ac504
叶璨铭 <12011404@mail.sustech.edu.cn>
2021-12-15 00:24:17 +0800

    添加项目文件。

---
+++ OnlineJudgeJavaSubmissionHelper/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace OnlineJudgeJavaSubmissionHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.syntaxBoxControl1 = new Alsing.Windows.Forms.SyntaxBoxControl();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(618, 47);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "choose java file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 48);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(516, 47);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(618, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 48);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2. #include \"header.java\"";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(618, 402);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(196, 48);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "3.public class Main";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(618, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(196, 48);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "1.remove package";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(618, 586);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(196, 117);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Bake All";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(618, 532);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(196, 48);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "copy";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // syntaxBoxControl1
+            // 
+            this.syntaxBoxControl1.ActiveView = Alsing.Windows.Forms.ActiveView.BottomRight;
+            this.syntaxBoxControl1.AutoListPosition = null;
+            this.syntaxBoxControl1.AutoListSelectedText = "a123";
+            this.syntaxBoxControl1.AutoListVisible = false;
+            this.syntaxBoxControl1.BackColor = System.Drawing.Color.Transparent;
+            this.syntaxBoxControl1.BorderStyle = Alsing.Windows.Forms.BorderStyle.None;
+            this.syntaxBoxControl1.CopyAsRTF = false;
+            this.syntaxBoxControl1.FontName = "Courier new";
+            this.syntaxBoxControl1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.syntaxBoxControl1.InfoTipCount = 1;
+            this.syntaxBoxControl1.InfoTipPosition = null;
+            this.syntaxBoxControl1.InfoTipSelectedIndex = 1;
+            this.syntaxBoxControl1.InfoTipVisible = false;
+            this.syntaxBoxControl1.LineNumberBackColor = System.Drawing.Color.Transparent;
+            this.syntaxBoxControl1.LineNumberBorderColor = System.Drawing.Color.Thistle;
+            this.syntaxBoxControl1.Location = new System.Drawing.Point(53, 102);
+            this.syntaxBoxControl1.LockCursorUpdate = false;
+            this.syntaxBoxControl1.Name = "syntaxBoxControl1";
+            this.syntaxBoxControl1.ShowScopeIndicator = false;
+            this.syntaxBoxControl1.Size = new System.Drawing.Size(516, 601);
+            this.syntaxBoxControl1.SmoothScroll = false;
+            this.syntaxBoxControl1.SplitView = false;
+            this.syntaxBoxControl1.SplitviewH = -3;
+            this.syntaxBoxControl1.SplitviewV = -3;
+            this.syntaxBoxControl1.TabGuideColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
+            this.syntaxBoxControl1.TabIndex = 8;
+            this.syntaxBoxControl1.Text = "syntaxBoxControl1";
+            this.syntaxBoxControl1.WhitespaceColor = System.Drawing.SystemColors.ControlDark;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 732);
+            this.Controls.Add(this.syntaxBoxControl1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Online Judge Java Submission Helper\t\t by 叶璨铭";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Alsing.Windows.Forms.SyntaxBoxControl syntaxBoxControl1;
+    }
+}
---
+++ OnlineJudgeJavaSubmissionHelper/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Alsing.SourceCode;
+
+namespace OnlineJudgeJavaSubmissionHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.DoWork += new DoWorkEventHandler(_readJavaCode);
+            backgroundWorker1.ProgressChanged += new ProgressChangedEventHandler(_readCodeProgessChanged);
+            backgroundWorker1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(_readCodeWorkCompleted);
+            // readJavaCode = new ReadJavaCode(_readJavaCode);
+        }
+
+        private void _readCodeWorkCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // throw new NotImplementedException();
+        }
+
+        private void _readCodeProgessChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // throw new NotImplementedException();
+        }
+
+        private readonly ComponentResourceManager _resources = new ComponentResourceManager(typeof(Form1));
+
+        // public delegate void ReadJavaCode();
+        // public ReadJavaCode readJavaCode;
+        public void _readJavaCode(object sender, DoWorkEventArgs e)
+        {
+            using (StreamReader sr = new StreamReader(textBox1.Text))
+            {
+                // syntaxBoxControl1.Text = sr.ReadToEndAsync().Result;
+                // var stream = sr.BaseStream;
+                // syntaxBoxControl1.Document = new SyntaxDocument();
+                // stream.Length+"";
+                var list = new List<string>();
+                int i = 0;
+                while (!sr.EndOfStream)
+                {
+                    list.Add(sr.ReadLine());
+                    backgroundWorker1.ReportProgress(i++);
+                }
+                syntaxBoxControl1.Document.Lines = list.ToArray();
+            }
+        }
+
+        private string lastLocation = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy)
+            {
+                return;
+            }
+            var dialog = new OpenFileDialog
+            {
+                Filter = @"java file|*.java|any file|*.*",
+                // Title = this._resources.GetString("Form1_button1_Click_Choose_Hint"),
+                Title = @"Please choose the java file that you want to submit.",
+                InitialDirectory = (lastLocation==null? Environment.CurrentDirectory : lastLocation ) 
+            };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.FileName;
+                lastLocation = Path.GetDirectoryName(dialog.FileName);
+                backgroundWorker1.RunWorkerAsync();
+                // this.BeginInvoke(readJavaCode);
+                // Thread thread = new Thread(new ThreadStart(_readJavaCode));
+                // thread.IsBackground = true;
+                // thread.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //一行只能include一次.
+        //预处理package和import
+        private string[] IncludeJava(string[] input)
+        {
+            var documentLines = new List<string>(input);
+            var imports = new List<string>();
+            for (int i = 0; i < documentLines.Count; i++)
+            {
+                //#include "test.java"
+                Match match = Regex.Match(documentLines[i], "//\\s*#\\s*include \"(?<file_name>.*?.java)\"");
+                if (match.Success)
+                {
+                    GroupCollection groups = match.Groups;
+                    var file_name = groups["file_name"].Value;
+                    // MessageBox.Show(file_name);
+                    if (lastLocation==null)
+                    {
+                        MessageBox.Show("Code not loaded");
+                    }
+                    // var directoryInfo = new DirectoryInfo(lastLocation);
+                    // var directoryInfo = new DirectoryInfo(lastLocation);
+                    try
+                    {
+                        using var streamReader = new StreamReader(Path.Combine(lastLocation, file_name));
+                        // var content = streamReader.ReadToEndAsync().Result;
+                        var content = new StringBuilder();
+                        while (!streamReader.EndOfStream)
+                        {
+                            var line = streamReader.ReadLine();
+                            if (Regex.IsMatch(line,"import .*?;"))
+                            {
+                                imports.Add(line);
+                            }
+                            else if(!Regex.IsMatch(line,"package .*?;"))
+                            {
+                                content.AppendLine(line);
+                            }
+                        }
+                        documentLines.RemoveAt(i);
+                        documentLines.Insert(i, content.ToString()); //前插.
+                    }
+                    catch (FileNotFoundException e)
+                    {
+                        MessageBox.Show("无法读取文件!\n" + e);
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            imports.AddRange(documentLines);
+            return imports.ToArray();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            syntaxBoxControl1.Enabled = false;
+            syntaxBoxControl1.Document.Lines = IncludeJava(syntaxBoxControl1.Document.Lines);
+            syntaxBoxControl1.Enabled = true;
+        }
+        //暴力删除，直接删掉一行
+        private string[] RemovePackage(string[] input)
+        {
+            var documentLines = new List<string>(input);
+            for (int i = 0; i < documentLines.Count; i++)
+            {
+                Match match = Regex.Match(documentLines[i], "package .*?;");
+                if (match.Success)
+                {
+                    documentLines.RemoveAt(i);
+                }
+            }
+            return documentLines.ToArray();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            syntaxBoxControl1.Enabled = false;
+            syntaxBoxControl1.Document.Lines = RemovePackage(syntaxBoxControl1.Document.Lines);
+            syntaxBoxControl1.Enabled = true;
+        }
+        private string[] ReplaceMain(string[] input)
+        {
+            // var documentLines = new List<string>(input);
+            // Regex reg = new Regex("//\\s*#pragma\\s+[Oo][Jj]\\s+[Mm]ain\n+public\\s+class\\s+\\S+\\s");
+            // for (int i = 0; i < documentLines.Count; i++)
+            // {
+            //     documentLines[i] = reg.Replace(documentLines[i], "public class Main ");
+            // }
+            // return documentLines.ToArray();
+            Regex reg = new Regex("//\\s*#\\s*pragma\\s+[Oo][Jj]\\s+[Mm]ain\\s*[\r\n]+public\\s+class\\s+\\S+\\s");
+            var @join = string.Join("\n", input);
+            return reg.Replace(@join, "public class Main ").Split("\n");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            syntaxBoxControl1.Enabled = false;
+            syntaxBoxControl1.Document.Lines = ReplaceMain(syntaxBoxControl1.Document.Lines);
+            syntaxBoxControl1.Enabled = true;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            syntaxBoxControl1.SelectAll();
+            syntaxBoxControl1.Copy();
+            MessageBox.Show(@"Copied!");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            syntaxBoxControl1.Enabled = false;
+            syntaxBoxControl1.Document.Lines = RemovePackage(IncludeJava(ReplaceMain(syntaxBoxControl1.Document.Lines)));
+            syntaxBoxControl1.Enabled = true;
+        }
+    }
+}
